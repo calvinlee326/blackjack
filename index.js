60a7572bb4a88dd03cfdad0010583000dd755043
calvinlee326 <calvinlee326@gmail.com>
2026-03-05 17:52:00 -0800

    Update button styling in CSS, modify HTML structure for card display, and refactor JavaScript game logic for improved functionality

--- index.js
+++ index.js
@@ -1,45 +1,41 @@
-let firstCard = Math.ceil(Math.random() * 11);
-console.log("First card: " + firstCard);
-let secondCard = Math.ceil(Math.random() * 11);
-console.log("Second card: " + secondCard);
-let sum = firstCard + secondCard;
-let isALive = true;
+let firstCard;
+let secondCard;
+let sum;
+let isAlive = false;
 let hasBlackjack = false;
-let message= "";
 
-// let sumEl = document.getElementById("sum-el");
-let sumEL= document.querySelector(".sum-el");
-let messageEL= document.querySelector("#message-el");
-let cardsEL= document.querySelector("#cards-el");
+let sumEL = document.querySelector("#sum-el");
+let messageEL = document.querySelector("#message-el");
+let cardsEL = document.querySelector("#cards-el");
 
-console.log("Sum: " + sum);
 function startGame() {
-    cardsEL.textContent = "Cards: " + firstCard + " " + secondCard;
-    sumEL.textContent = "Sum: " + sum;
-    if (sum < 21) {
-        console.log("Do you want to draw a new card?");
-    } else if (sum === 21) {
-    console.log("You've got Blackjack!");
-    hasBlackjack = true;
-    } else if (sum > 21) {
-    isALive = false; //player busted
-    console.log("You're out of the game!");
-    } 
-    console.log("Has blackjack: " + hasBlackjack);
-    console.log("Is alive: " + isALive);
+    firstCard = Math.ceil(Math.random() * 11);
+    secondCard = Math.ceil(Math.random() * 11);
+    sum = firstCard + secondCard;
+    isAlive = true;
+    hasBlackjack = false;
+    cardsEL.textContent = firstCard + " " + secondCard;
+    renderGame();
 }
+
 function newCard() {
-    console.log("Drawing a new card from the deck!");
+    if (!isAlive || hasBlackjack) return;
     let card = Math.ceil(Math.random() * 11);
     sum += card;
-    cardsEL.textContent = "Cards: " + firstCard + " " + secondCard + " " + card;
-    sumEL.textContent = "Sum: " + sum;
+    cardsEL.textContent += " " + card;
+    renderGame();
+}
+
+function renderGame() {
+    sumEL.textContent = sum;
     if (sum < 21) {
-        console.log("Do you want to draw a new card?");
+        messageEL.textContent = "Do you want to draw a new card?";
     } else if (sum === 21) {
-        console.log("You've got Blackjack!");
-    } else if (sum > 21) {
-        console.log("You're out of the game!");
+        messageEL.textContent = "You've got Blackjack!";
+        hasBlackjack = true;
+    } else {
+        messageEL.textContent = "You're out of the game!";
+        isAlive = false;
     }
 }
 
