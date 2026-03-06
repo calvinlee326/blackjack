0a5a6a6b6e4e0bc55e5e2783035bf63c16bc11c8
calvinlee326 <calvinlee326@gmail.com>
2026-03-05 17:39:06 -0800

    Refactor HTML structure, enhance JavaScript functionality, and add button styling for Blackjack game

--- index.js
+++ index.js
@@ -5,17 +5,31 @@ console.log("Second card: " + secondCard);
 let sum = firstCard + secondCard;
 let isALive = true;
 let hasBlackjack = false;
+let message= "";
+
+// let sumEl = document.getElementById("sum-el");
+let sumEL= document.querySelector(".sum-el");
+let messageEL= document.querySelector("#message-el");
+let cardsEL= document.querySelector("#cards-el");
 
 console.log("Sum: " + sum);
-
-if (sum < 21) {
-    console.log("Do you want to draw a new card?");
-} else if (sum === 21) {
+function startGame() {
+    cardsEL.textContent = "Cards: " + firstCard + " " + secondCard;
+    sumEL.textContent = "Sum: " + sum;
+    if (sum < 21) {
+        console.log("Do you want to draw a new card?");
+    } else if (sum === 21) {
     console.log("You've got Blackjack!");
     hasBlackjack = true;
-} else {
+    } else if (sum > 21) {
     isALive = false; //player busted
     console.log("You're out of the game!");
-} 
+    } 
+    console.log("Has blackjack: " + hasBlackjack);
+    console.log("Is alive: " + isALive);
+}
+function newCard() {
+    console.log("Drawing a new card from the deck!");
+}
 
-console.log("Has blackjack: " + hasBlackjack); 
+ 
